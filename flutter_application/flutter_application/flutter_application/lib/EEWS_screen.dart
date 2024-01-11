943c92b27a244a1e6a9b1319a19dd56cacff3c8a
dodimarthen <dodimartin.sit@gmail.com>
2024-01-11 11:06:11 +0700

    first code

---
+++ flutter_application/flutter_application/flutter_application/lib/EEWS_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EEWSscreen extends StatefulWidget {
+  const EEWSscreen({super.key});
+
+  @override
+  State<EEWSscreen> createState() => _EEWSscreenState();
+}
+
+class _EEWSscreenState extends State<EEWSscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
